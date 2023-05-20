ef61e8bbf77240ad5944ca60d7ee636413c2d818
gilang <kadekgilangputra@gmail.com>
2023-05-20 22:13:10 +0800

    refactor and add env variable for merchant id

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:purala/home/home_screen.dart';
 import 'package:purala/signin/signin_screen.dart';
+import 'package:purala/starter/starter_screen.dart';
 
 var routes = <String, WidgetBuilder>{
-  HomeScreen.routeName: (_) => const HomeScreen(),
+  StarterScreen.routeName: (_) => const StarterScreen(),
   SigninScreen.routeName: (_) => const SigninScreen(),
+  HomeScreen.routeName: (_) => const HomeScreen(),
 };
